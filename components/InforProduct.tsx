7d53ec4db00590cc392f964acfc5c4723ab83a90
Đoàn Gia Bảo <giabaod345@gmail.com>
2023-10-09 21:53:04 +0700

    change

--- components/InforProduct.tsx
+++ components/InforProduct.tsx
@@ -182,33 +182,33 @@ const InforProduct = () => {
               <span className="text-[14px] font-normal text-[Gray]">
                 Mile stone-2
               </span>
-              <div></div>
-              <div className="flex items-center border ml-[200px] border-[#F03738]/[0.5] rounded-[5px] px-[8px] py-[4px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="8.00033"
-                    cy="7.99984"
-                    rx="3.33334"
-                    ry="3.33333"
-                    fill="#F03738"
-                  />
-                </svg>
-                <span className="text-[15px] ml-[5px]  text-[#F03738]">
-                  Unpaid
-                </span>
+              <div className="flex space-x-5">
+                <div className="flex items-center border ml-[200px] border-[#F03738]/[0.5] rounded-[5px] px-[8px] py-[4px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <ellipse
+                      cx="8.00033"
+                      cy="7.99984"
+                      rx="3.33334"
+                      ry="3.33333"
+                      fill="#F03738"
+                    />
+                  </svg>
+                  <span className="text-[15px] ml-[5px]  text-[#F03738]">
+                    Unpaid
+                  </span>
+                </div>
+                <button className="bg-[#6D6CE3]  text-[white] rounded-[28px] px-[21px] py-[10px]">
+                  Play now
+                </button>{" "}
               </div>
-              <button className="bg-[#6D6CE3]  text-[white] rounded-[28px] px-[21px] py-[10px]">
-                Play now
-              </button>{" "}
             </p>{" "}
           </div>
-
           <div className="card h-[150px] w-full rounded-[10px] mt-[20px] p-[20px] bg-[#FFF]">
             <h3 className="text-[18px] flex justify-between text-[#212121] font-medium">
               Dashboard development
@@ -226,30 +226,31 @@ const InforProduct = () => {
               <span className="text-[14px] font-normal text-[Gray]">
                 Mile stone-3
               </span>
-              <div></div>
-              <div className="flex items-center border ml-[200px] border-[#F03738]/[0.5] rounded-[5px] px-[8px] py-[4px]">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 16 16"
-                  fill="none"
-                >
-                  <ellipse
-                    cx="8.00033"
-                    cy="7.99984"
-                    rx="3.33334"
-                    ry="3.33333"
-                    fill="#F03738"
-                  />
-                </svg>
-                <span className="text-[15px] ml-[5px]  text-[#F03738]">
-                  Unpaid
-                </span>
+              <div className="flex space-x-5">
+                <div className="flex items-center border ml-[200px] border-[#F03738]/[0.5] rounded-[5px] px-[8px] py-[4px]">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                  >
+                    <ellipse
+                      cx="8.00033"
+                      cy="7.99984"
+                      rx="3.33334"
+                      ry="3.33333"
+                      fill="#F03738"
+                    />
+                  </svg>
+                  <span className="text-[15px] ml-[5px]  text-[#F03738]">
+                    Unpaid
+                  </span>
+                </div>
+                <button className="bg-[#6D6CE3]  text-[white] rounded-[28px] px-[21px] py-[10px]">
+                  Play now
+                </button>{" "}
               </div>
-              <button className="bg-[#6D6CE3]  text-[white] rounded-[28px] px-[21px] py-[10px]">
-                Play now
-              </button>{" "}
             </p>{" "}
           </div>
         </div>
